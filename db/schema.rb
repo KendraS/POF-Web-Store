cc7f1154ff76528eed70478c9388d6e950cd2ab8
Kendra Sys <kendra_s@mts.net>
2014-11-21 08:48:02 -0600

    Added image column to products table and modified Gemfile to prepare ActiveAdmin setup.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121142204) do
+ActiveRecord::Schema.define(version: 20141121144521) do
 
   create_table "categories", force: true do |t|
     t.string   "category_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20141121142204) do
     t.integer  "stock_quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
