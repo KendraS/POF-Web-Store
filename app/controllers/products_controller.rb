f1d4046b283971337c0c3d3154a2751ff72b3f6f
Kendra Sys <kendra_s@mts.net>
2014-11-21 12:21:58 -0600

    Added wet/dry food and treats action to products controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,24 @@
 class ProductsController < ApplicationController
 	def index
+		@page_title = 'All Products'
 		@products = Product.all
 	end
 	# The @products variable will now be accessible from the
 	# automatically loaded view: app/views/products/index.html.erb
+
+	def dry_food
+
+	end
+
+
+	def wet_food
+
+	end
+
+
+	def biscuits_and_treats
+
+	end
 
 
 end
