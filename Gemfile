af44dc82b7cafc4b49c8a2dc986b5fedf3a2eb96
Kendra Sys <kendra_s@mts.net>
2014-11-22 23:39:59 -0600

    Added comments regarding 'Carrierwave' to product model - need to debug.

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 gem 'activeadmin', github: 'activeadmin'
 gem 'devise'
-
+gem 'carrierwave'
 
 
 # Use ActiveModel has_secure_password
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,16 @@
 class Product < ActiveRecord::Base
-	belongs_to :category
+	# Carrierwave
+	#mount_uplaoder :image, ImageUploader
 
+	belongs_to :category
+	
 	validates :product_name, :product_description, :unit_price, :retail_price,
 			  :stock_quantity, presence: true
 	validates :stock_quantity, numericality: { only_integer: true }
 	validates :unit_price, :retail_price, numericality: { greater_than_or_equal_to: 0.01 }
 
 	# Add method on the class
-	def self.keyword_search
+	def self.keyword_search(keywords)
 		keywords = "%" + keywords + "%"
 		Product.where("product_name LIKE ? OR product_description LIKE ?", keywords, keywords)
 	end
