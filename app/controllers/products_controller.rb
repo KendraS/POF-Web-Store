8809760a39a69271ac985a5bfff329732f8c083e
Kendra Sys <kendra_s@mts.net>
2014-11-22 22:03:29 -0600

    Deployed functionality to utilize Kaminari's paginate helper and added pagination to products view.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
 	def index
 		@page_title = 'All Products'
-		@products = Product.all
+		#@products = Product.all
+		@products = Product.order("product_name").page(params[:page]).per(5)  
 	end
 
 
